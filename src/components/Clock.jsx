54fd6fa9859cbf0cbc20f7bf3abe9dfb16115ed1
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2017-03-24 00:42:33 -0400

    Fix dependencies categorization

---
+++ src/components/Clock.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <h1>Clock component!</h1>
+    );
+  }
+}
+
+export default Clock;
